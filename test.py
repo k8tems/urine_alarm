f4d69f7d1ccf037085e07751f58a9b9069adeba4
k8tem <k8tems@live.com>
2024-11-16 13:19:27 +0900

    Add failing test for get_sheet

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import unittest
+from datetime import datetime
+import main
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestGetSheet(unittest.TestCase):
+    def test(self):
+        ss = main.get_spread_sheet(main.SPREAD_TITLE)
+        now = datetime(2024, 1, 6)
+        sheet = main.get_worksheet(ss, now)
+        self.assertEqual('01/05', sheet.title)
+
 
 if __name__ == '__main__':
     unittest.main()
